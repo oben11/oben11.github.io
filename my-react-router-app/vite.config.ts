fb134ac91e1e1d8fa9237909057053a2fe84c424
oben11 <moolock.moo@gmail.com>
2026-08-27 23:19:27 -0400

    Attempting to deploy to pages

--- my-react-router-app/vite.config.ts
+++ my-react-router-app/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/oben11,github.io/',
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
